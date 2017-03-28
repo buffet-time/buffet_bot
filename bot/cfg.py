4bb67b0afef117dd05dbff793d041df463ff819b
Buffet Time <buffet-time@users.noreply.github.com>
2017-03-28 12:06:23 -0400

    Created cfg.py

---
+++ bot/cfg.py
@@ -0,0 +1,34 @@
+# Config File
+
+HOST = "irc.twitch.tv"                          # irc server
+PORT = 6667                                     # irc port#
+NICK = "buffet_bot"                             # twitch name (lower case)
+PASS = "[REDACTED]"                             # OAuth Token
+CHAN = "#buffet_time"                           # channel to connect to
+
+# Not Thomas Command
+NTC = [
+        r"not_thomas_123",
+        r"@not_thomas_123"
+        r"not thomas 123",
+        r"not underscore thomas underscore 123",
+        r"notthomas",
+        r"not_thomas",
+        r"not thomas",
+        r"not underscore thomas"
+]
+
+# Meme%
+MP = [
+    r"meme%",
+    r"meme percent",
+    r"m3m3%",
+    r"mem%"
+]
+
+# Rolling 2 dice
+RN = [
+    r"!roll",
+    r"!ROLL",
+    r"!Roll"
+]
